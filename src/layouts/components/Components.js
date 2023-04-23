93a191be4f6c01d826531fc65831b601d770304d
diana <dinazhusubalieva@gmail.com>
2023-04-23 16:30:55 +0600

    add custom transition component

--- src/layouts/components/Components.js
+++ src/layouts/components/Components.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./styles.scss";
 import Dialog from "./Dialog";
+import MyTransition from "./transition/MyTransition";
 
 const CornerDialog = ({ open }) => {
   return (
@@ -13,15 +14,30 @@ const CornerDialog = ({ open }) => {
 const Components = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
+  const [openTest, setOpenTest] = useState(false);
   return (
     <div className={"components-wrapper"}>
       <div className={"components-content"}>
-        <button
-          onClick={() => setIsDialogOpen(!isDialogOpen)}
-          className={"basic-button"}
-        >
-          open modal dialog
-        </button>
+        <div className={"grid-container"}>
+          <button
+            onClick={() => setIsDialogOpen(!isDialogOpen)}
+            className={"basic-button"}
+          >
+            open modal dialog
+          </button>
+
+          <button
+            className={"basic-button"}
+            onClick={() => setOpenTest(!openTest)}
+          >
+            open block
+          </button>
+        </div>
+        <MyTransition in={openTest}>
+          <div
+            style={{ background: "blue", width: "100px", height: "50px" }}
+          ></div>
+        </MyTransition>
       </div>
       <CornerDialog open={isDialogOpen} />
     </div>
